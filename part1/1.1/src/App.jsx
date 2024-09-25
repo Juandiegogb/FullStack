898e86c9757e7db0d291e85727646b7d8281ac7a
Juan Diego <juandiegar@gmail.com>
2024-09-25 15:19:04 -0500

    1.7

--- part1/1.1/src/App.jsx
+++ part1/1.1/src/App.jsx
@@ -1,69 +1,47 @@
+import { useState } from "react";
+
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [punctuation, setPunctuation] = useState(0);
+
+  const newGood = () => {
+    setGood(good + 1);
+    setPunctuation(punctuation + 1);
+    setAll(all + 1);
+  };
+
+  const newNeutral = () => {
+    setNeutral(neutral + 1);
+    setAll(all + 1);
+  };
+
+  const newBad = () => {
+    setBad(bad + 1);
+    setPunctuation(punctuation - 1);
+    setAll(all + 1);
   };
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total props={course.parts} />
+      <h1>Give feedback</h1>
+      <Button text="Good" onSmash={newGood} />
+      <Button text="Neutral" onSmash={newNeutral} />
+      <Button text="Bad" onSmash={newBad} />
+
+      <h2>Statistics</h2>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>All {all}</p>
+      <p>Average {punctuation / all}</p>
+      <p>Positive {(good/all)*100} %</p>
     </div>
   );
 };
 
-const Header = ({ course }) => {
-  return (
-    <div>
-      <h1>{course}</h1>
-    </div>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part, index) => (
-        <Part key={index} part={part} />
-      ))}
-    </div>
-  );
-};
-
-const Total = ({ props }) => {
-  let total = 0;
-  props.map((part) => {
-    total += part.exercises;
-  });
-  return (
-    <div>
-      <p>Number of exercises {total}</p>
-    </div>
-  );
-};
-
-const Part = ({ part }) => {
-  return (
-    <div>
-      <p>
-        {part.name} {part.excercises}
-      </p>
-    </div>
-  );
-};
+const Button = ({ text, onSmash }) => <button onClick={onSmash}>{text}</button>;
 
 export default App;
