f1b260aea3be4dcbd0838c8904bd782c3bf7cd38
Juan Diego <juandiegar@gmail.com>
2024-09-25 14:01:33 -0500

    1.3 - 1.4 - 1.5

--- part1/1.1/src/App.jsx
+++ part1/1.1/src/App.jsx
@@ -1,29 +1,69 @@
-import { useEffect, useState } from "react";
-
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const newNeutral = () => setNeutral(neutral + 1);
-  const newGood = () => setGood(good + 1);
-  const newBad = () => setBad(bad + 1);
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <h1>Give feedback</h1>
-      <Button text="Good" onSmash={newGood} />
-      <Button text="Neutral" onSmash={newNeutral} />
-      <Button text="Bad" onSmash={newBad} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total props={course.parts} />
+    </div>
+  );
+};
 
-      <h2>Statistics</h2>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
+const Header = ({ course }) => {
+  return (
+    <div>
+      <h1>{course}</h1>
+    </div>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ props }) => {
+  let total = 0;
+  props.map((part) => {
+    total += part.exercises;
+  });
+  return (
+    <div>
+      <p>Number of exercises {total}</p>
+    </div>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <div>
+      <p>
+        {part.name} {part.excercises}
+      </p>
     </div>
   );
 };
 
 export default App;
-
-const Button = ({ text, onSmash }) => <button onClick={onSmash}>{text}</button>;
