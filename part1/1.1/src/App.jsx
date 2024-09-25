3d400c17d600006f5b83d348c17d32243b903038
Juan Diego <juandiegar@gmail.com>
2024-09-25 13:57:14 -0500

    1.6

--- part1/1.1/src/App.jsx
+++ part1/1.1/src/App.jsx
@@ -1,61 +1,29 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const newNeutral = () => setNeutral(neutral + 1);
+  const newGood = () => setGood(good + 1);
+  const newBad = () => setBad(bad + 1);
 
   return (
     <div>
-      <Header course={course} />
-      <Content
-        props={{ part1, exercises1, part2, exercises2, part3, exercises3 }}
-      />
-      <Total props={{ exercises1, exercises2, exercises3 }} />
-    </div>
-  );
-};
+      <h1>Give feedback</h1>
+      <Button text="Good" onSmash={newGood} />
+      <Button text="Neutral" onSmash={newNeutral} />
+      <Button text="Bad" onSmash={newBad} />
 
-const Header = ({ course }) => {
-  return (
-    <div>
-      <h1>{course}</h1>
-    </div>
-  );
-};
-
-const Content = ({ props }) => {
-  console.log(props);
-  return (
-    <div>
-      <Part part={props.part1} excercises={props.exercises1} />
-      <Part part={props.part2} excercises={props.exercises2} />
-      <Part part={props.part3} excercises={props.exercises3} />
-    </div>
-  );
-};
-
-const Total = ({ props }) => {
-  return (
-    <div>
-      <p>
-        Number of exercises{" "}
-        {props.exercises1 + props.exercises2 + props.exercises3}
-      </p>
-    </div>
-  );
-};
-
-const Part = ({ part, excercises }) => {
-  return (
-    <div>
-      <p>
-        {part} {excercises}
-      </p>
+      <h2>Statistics</h2>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
     </div>
   );
 };
 
 export default App;
+
+const Button = ({ text, onSmash }) => <button onClick={onSmash}>{text}</button>;
