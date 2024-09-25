6ad62f6fd761d7c4ad64f2098ef5920bd6df4e75
Juan Diego <juandiegar@gmail.com>
2024-09-25 16:39:05 -0500

    message: "Ejercicios"

--- part1/1.1/src/App.jsx
+++ part1/1.1/src/App.jsx
@@ -1,89 +1,46 @@
 import { useState } from "react";
 
 const App = () => {
-  const [data, setData] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    all: 0,
-    punctuation: null,
-  });
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
 
-  const newGood = () => {
-    setData({
-      ...data,
-      good: data.good + 1,
-      punctuation: data.punctuation + 1,
-      all: data.all + 1,
-    });
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0));
+  const [selected, setSelected] = useState(0);
+
+  const next = () => {
+    const max = anecdotes.length;
+    const index = Math.floor(Math.random() * max);
+    setSelected(index);
+  };
+  const vote = () => {
+    const copy = [...points];
+    copy[selected] += 1;
+    setPoints(copy);
   };
 
-  const newNeutral = () => {
-    setData({
-      ...data,
-      neutral: data.neutral + 1,
-      all: data.all + 1,
-    });
-  };
-
-  const newBad = () => {
-    setData({
-      ...data,
-      bad: data.bad + 1,
-      punctuation: data.punctuation - 1,
-      all: data.all + 1,
-    });
+  const mostVoted = () => {
+    const maxVotes = Math.max(...points);
+    const index = points.indexOf(maxVotes);
+    return anecdotes[index] + ` Has ${maxVotes} votes.`;
   };
 
   return (
     <div>
-      <h1>Give feedback</h1>
-      <Button text="Good" onSmash={newGood} />
-      <Button text="Neutral" onSmash={newNeutral} />
-      <Button text="Bad" onSmash={newBad} />
-      <Statistics data={data} />
+      {anecdotes[selected]}
+      <p>Has {points[selected]} votes </p>
+      <button onClick={vote}>Vote</button>
+      <button onClick={next}>Next</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{mostVoted()}</p>
     </div>
-  );
-};
-
-const Button = ({ text, onSmash }) => <button onClick={onSmash}>{text}</button>;
-const StatisticLine = ({ text, value }) => (
-  <tr>
-    <td>{text}</td>
-    <td>{value}</td>
-  </tr>
-);
-
-const Statistics = ({ data }) => {
-  return data.punctuation !== null ? (
-    <div>
-      <h2>Statistics</h2>
-
-      <div>
-        <h2>Statistics</h2>
-        <table>
-          <thead>
-            <tr>
-              <td>Description</td>
-              <td>Data</td>
-            </tr>
-          </thead>
-          <tbody>
-            <StatisticLine text="Good" value={data.good} />
-            <StatisticLine text="Neutral" value={data.neutral} />
-            <StatisticLine text="Bad" value={data.bad} />
-            <StatisticLine text="All" value={data.all} />
-            <StatisticLine text="Average" value={data.punctuation / data.all} />
-            <StatisticLine
-              text="Positive"
-              value={(data.good / data.all) * 100}
-            />
-          </tbody>
-        </table>
-      </div>
-    </div>
-  ) : (
-    <p>No feedback given</p>
   );
 };
 
