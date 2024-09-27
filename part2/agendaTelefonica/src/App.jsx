318fa90805ebbac2ed1585ff1edeb2cf1660bcdc
Juan Diego <juandiegar@gmail.com>
2024-09-26 21:23:16 -0500

    agenda 2.10

--- part2/agendaTelefonica/src/App.jsx
+++ part2/agendaTelefonica/src/App.jsx
@@ -1,21 +1,20 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", phone: 12345 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState(0);
-  const [findName, setFindName] = useState(null);
-  const [filteredPersons, setFilteredPersons] = useState(null);
+  const [toShow, setToShow] = useState(null);
 
-  const findHandler = (e) => {
-    console.log(e.target.value);
-    setFindName(e.target.value);
-    const filtered = persons.filter((person) =>
-      person.name.toLowerCase().includes(findName.toLowerCase())
+  const finderHandler = (e) => {
+    const inputValue = e.target.value;
+    setToShow(
+      persons.filter((person) =>
+        person.name.toLowerCase().includes(inputValue.toLowerCase())
+      )
     );
-    e.target.value.lenght === ""
-      ? setFilteredPersons(null)
-      : setFilteredPersons(filtered);
   };
 
   const nameHandler = (e) => {
@@ -38,6 +37,7 @@ const App = () => {
       e.target.reset();
       setNewName("");
       console.log(persons);
+      setToShow(null);
       return;
     }
     alert(`${newName} is already added to phonebook`);
@@ -47,31 +47,52 @@ const App = () => {
   return (
     <div>
       <h2>Search</h2>
-      <input type="text" placeholder="Filter contacts" onChange={findHandler} />
+      <Finder customOnChange={finderHandler} />
       <h2>Phonebook</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          name: <input onChange={nameHandler} required />
-          Phone: <input type="tel" onChange={phoneHandler} required />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {filteredPersons
-        ? filteredPersons.map((person, i) => (
-            <p key={i}>
-              <strong>{person.name}</strong> {person.phone}
-            </p>
-          ))
-        : persons.map((person, i) => (
-            <p key={i}>
-              <strong>{person.name}</strong> {person.phone}
-            </p>
-          ))}
+      <AddContactForm
+        inputNameHandler={nameHandler}
+        onSubmitForm={handleSubmit}
+        inputPhoneHandler={phoneHandler}
+      />
+      <h2>Contacts</h2>
+      <Contacts toShow={toShow} persons={persons} />
     </div>
   );
 };
-
 export default App;
+
+const Finder = ({ customOnChange }) => (
+  <input type="text" placeholder="Filter contacts" onChange={customOnChange} />
+);
+
+const AddContactForm = ({
+  onSubmitForm,
+  inputNameHandler,
+  inputPhoneHandler,
+}) => {
+  return (
+    <form onSubmit={onSubmitForm}>
+      <div>
+        name: <input onChange={inputNameHandler} required />
+        Phone: <input type="tel" onChange={inputPhoneHandler} required />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+const Contacts = ({ toShow, persons }) => {
+  return toShow
+    ? toShow.map((person, i) => (
+        <p key={i}>
+          <strong>{person.name}</strong> {person.phone}
+        </p>
+      ))
+    : persons.map((person, i) => (
+        <p key={i}>
+          <strong>{person.name}</strong> {person.phone}
+        </p>
+      ));
+};
